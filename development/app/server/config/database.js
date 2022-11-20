cacaeded59666a17f1bd1cfd31e1b708f1bf6e8a
Sabine Adam <s80447@informatik.htw-dresden.de>
2022-11-21 00:11:05 +0100

    edit

--- development/app/server/config/database.js
+++ development/app/server/config/database.js
@@ -1,8 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("trommelhelden", "sa", "Trommelhelden24!", {
-  host: "localhost",
-  port: 1433,
+const db = new Sequelize("ii19s80447", "s80447", "s80447", {
+  host: "141.56.2.45",
   dialect: "mssql",
   operatorAliases: false,
   pool: {
