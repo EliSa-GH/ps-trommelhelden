26a3f8ee7ac372b02ba4c23252a481ed62993a41
qduyp <92373381+qduyp@users.noreply.github.com>
2022-12-04 17:54:55 +0100

    create a job

--- development/app/server/config/database.js
+++ development/app/server/config/database.js
@@ -1,19 +1,6 @@
 import { Sequelize } from "sequelize";
 
-/* const db = new Sequelize("ii19s80431", "s80431", "s80431", {
-  host: "141.56.2.45",
-  dialect: "mssql",
-  operatorAliases: false,
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 1000,
-  },
-});
-export default db; */
-
-const db = new Sequelize("ii20s82050", "s82050", "s82050", {
+const db = new Sequelize("ii19s80431", "s80431", "s80431", {
   host: "141.56.2.45",
   dialect: "mssql",
   operatorAliases: false,
@@ -25,6 +12,19 @@ const db = new Sequelize("ii20s82050", "s82050", "s82050", {
   },
 });
 export default db;
+
+/* const db = new Sequelize("ii20s82050", "s82050", "s82050", {
+  host: "141.56.2.45",
+  dialect: "mssql",
+  operatorAliases: false,
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 1000,
+  },
+});
+export default db; */
 
 // const db = new Sequelize("trommelhelden", "sa", "Trommelhelden24!", {
 //   host: "localhost",
